af3b4b5df1f2aea40ebd4f56096c975dd1146edb
nomadqwerty <davexmike72@gmail.com>
2023-03-29 21:50:02 +0100

    staticAndDynamicCache

--- course-project-starting-setup/public/sw.js
+++ course-project-starting-setup/public/sw.js
@@ -1,8 +1,17 @@
 oninstall = async function (e) {
   try {
     const swCache = await caches.open("static");
-    console.log(swCache);
-    swCache.add("./src/js/app.js");
+
+    swCache.addAll([
+      "./",
+      "./index.html",
+      "./src/css/app.css",
+      "./src/js/app.js",
+      "./src/js/feed.js",
+      "./src/js/material.min.js",
+      "./src/js/fetch.js",
+      "./src/js/promise.js",
+    ]);
   } catch (error) {
     console.log(error.message);
   }
@@ -10,6 +19,43 @@ oninstall = async function (e) {
 onactivate = function (e) {
   clients.claim();
 };
+// onfetch = async function (e) {
+//   e.respondWith(
+//     caches
+//       .match(e.request)
+//       .then((res) => {
+//         if (res) {
+//           return res;
+//         } else {
+//           return fetch(e.request);
+//         }
+//       })
+//       .catch((err) => console.log(err.message))
+//   );
+// };
 onfetch = function (e) {
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    (async () => {
+      const cacheStore = await caches.match(e.request);
+      if (cacheStore) {
+        console.log("offline ");
+        return cacheStore;
+      } else if (!cacheStore) {
+        console.log("not yet cached");
+        if (!e.request.url.includes("chrome-extension")) {
+          const newCacheStore = await caches.open("dynamic");
+          newCacheStore.add(e.request.url);
+          const newCacheData = await newCacheStore.match(e.request);
+          if (newCacheData) {
+            console.log("cached and returned");
+            return newCacheData;
+          }
+        } else {
+          console.log("extension");
+          const res = await fetch(e.request);
+          return res;
+        }
+      }
+    })()
+  );
 };
