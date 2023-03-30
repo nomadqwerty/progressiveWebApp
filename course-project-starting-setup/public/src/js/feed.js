a17cf6039d015ca5f4433225a6861358ffb5a7fe
nomadqwerty <davexmike72@gmail.com>
2023-03-31 00:27:02 +0100

    advancedCache

--- course-project-starting-setup/public/src/js/feed.js
+++ course-project-starting-setup/public/src/js/feed.js
@@ -37,16 +37,32 @@ function createCard() {
   cardWrapper.appendChild(cardTitle);
   var cardTitleTextElement = document.createElement("h2");
   cardTitleTextElement.className = "mdl-card__title-text";
-  cardTitleTextElement.textContent = "San Francisco Trip";
+  cardTitleTextElement.textContent = "west Francisco Trip";
+  cardTitleTextElement.style.color = "yellow";
   cardTitle.appendChild(cardTitleTextElement);
   var cardSupportingText = document.createElement("div");
   cardSupportingText.className = "mdl-card__supporting-text";
   cardSupportingText.textContent = "In San Francisco";
   cardSupportingText.style.textAlign = "center";
+  if (window.caches) {
+    const btn = document.createElement("button");
+    btn.textContent = "save";
+    btn.addEventListener("click", async function (e) {
+      console.log("clicked");
+      const userCache = await caches.open("userV1");
+      userCache.addAll([
+        "https://httpbin.org/get",
+        "../../src/images/main-image-sm.jpg",
+      ]);
+    });
+    cardSupportingText.appendChild(btn);
+  }
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
 }
+
+console.log(window);
 
 fetch("https://httpbin.org/get")
   .then(function (res) {
--- course-project-starting-setup/public/sw.js
+++ course-project-starting-setup/public/sw.js
@@ -1,60 +1,58 @@
-oninstall = async function (e) {
-  try {
-    const swCache = await caches.open("static");
+const cacheName = "staticV1";
+const dynamicName = "dynamicV1";
 
-    swCache.addAll([
-      "./",
-      "./index.html",
-      "./src/css/app.css",
-      "./src/js/app.js",
-      "./src/js/feed.js",
-      "./src/js/material.min.js",
-      "./src/js/fetch.js",
-      "./src/js/promise.js",
-    ]);
-  } catch (error) {
-    console.log(error.message);
-  }
+oninstall = (e) => {
+  console.log("[Service Worker] Installing Service Worker ...");
+
+  e.waitUntil(
+    (async () => {
+      const staticStore = await caches.open(cacheName);
+      console.log("[Service Worker] Precaching App Shell");
+      staticStore.addAll([
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/promise.js",
+        "/src/js/fetch.js",
+        "/src/js/material.min.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+      ]);
+    })()
+  );
 };
-onactivate = function (e) {
-  clients.claim();
+onactivate = (e) => {
+  console.log("[Service Worker] Activating Service Worker ....");
+  e.waitUntil(
+    (async () => {
+      const cacheArray = await caches.keys();
+      cacheArray.forEach((key, i) => {
+        if (key !== cacheName && key !== dynamicName) {
+          console.log(key);
+          caches.delete(key);
+        }
+      });
+    })()
+  );
+  return self.clients.claim();
 };
-// onfetch = async function (e) {
-//   e.respondWith(
-//     caches
-//       .match(e.request)
-//       .then((res) => {
-//         if (res) {
-//           return res;
-//         } else {
-//           return fetch(e.request);
-//         }
-//       })
-//       .catch((err) => console.log(err.message))
-//   );
-// };
-onfetch = function (e) {
+
+onfetch = (e) => {
   e.respondWith(
     (async () => {
       const cacheStore = await caches.match(e.request);
       if (cacheStore) {
-        console.log("offline ");
         return cacheStore;
-      } else if (!cacheStore) {
-        console.log("not yet cached");
-        if (!e.request.url.includes("chrome-extension")) {
-          const newCacheStore = await caches.open("dynamic");
-          newCacheStore.add(e.request.url);
-          const newCacheData = await newCacheStore.match(e.request);
-          if (newCacheData) {
-            console.log("cached and returned");
-            return newCacheData;
-          }
-        } else {
-          console.log("extension");
-          const res = await fetch(e.request);
-          return res;
-        }
+      } else {
+        const fetchRes = await fetch(e.request);
+        const dynamicStore = await caches.open(dynamicName);
+        dynamicStore.put(e.request.url, fetchRes.clone());
+        return fetchRes;
       }
     })()
   );
