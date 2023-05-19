8786340aa0d63f5fb9db0b4495ecb69f27d7a09e
nomadqwerty <davexmike72@gmail.com>
2023-05-19 08:52:51 +0100

    cyber

--- course-project-starting-setup/public/src/js/app.js
+++ course-project-starting-setup/public/src/js/app.js
@@ -1,6 +1,20 @@
 window.promptEvent = null;
 const enableNotification = document.querySelectorAll(".enable-notifications");
 
+const url64To8bitArr = (url64) => {
+  let padding = "=".repeat((4 - (url64.length % 4)) % 4);
+  let base64 = (url64 + padding).replace(/\-/g, "+").replace(/_/g, "/");
+
+  let rawData = window.atob(base64);
+  let outPutArr = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; i++) {
+    outPutArr[i] = rawData.charCodeAt(i);
+  }
+  console.log(outPutArr);
+  return outPutArr;
+};
+
 // add polyfills ffrom app.js
 if (!window.Promise) {
   window.Promise = Promise;
@@ -74,8 +88,34 @@ const pushSubscription = async () => {
       const subs = await swReg.pushManager.getSubscription();
 
       console.log(subs);
-      if (!subs) {
-        // create a new subscription.
+      if (true) {
+        try {
+          // create a new subscription.
+          const pubKey =
+            "BJTnb4WHULLLnmRmPOhqrf6rQGJ7KYmcQ3XNzieAvFqtomw47Je3uFJnLtI1UIzekfLRjm3stdOejndX81vKUqs";
+
+          let pubKeyConvert = url64To8bitArr(pubKey);
+
+          const subscription = await swReg.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: pubKeyConvert,
+          });
+
+          const res = await fetch("http://192.168.8.148:3000/subs", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+              "Access-Control-Allow-Headers": "*",
+            },
+            body: JSON.stringify(subscription),
+          });
+
+          console.log(res);
+          await showNotification();
+        } catch (error) {
+          console.log(error);
+        }
       } else {
         // use the existing subscription.
       }
@@ -89,7 +129,7 @@ const getNotificationPermission = async (e) => {
   if (isGranted === "granted") {
     console.log("permision granted");
     e.target.style.display = "none";
-    await showNotification();
+    await pushSubscription();
   }
 };
 
--- server.js
+++ server.js
@@ -1,7 +1,39 @@
 const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const buffer = require("buffer");
 
-http
-  .createServer((req, res) => {
-    res.end("back end");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
   })
-  .listen(3000);
+);
+const publicKey =
+  "BJTnb4WHULLLnmRmPOhqrf6rQGJ7KYmcQ3XNzieAvFqtomw47Je3uFJnLtI1UIzekfLRjm3stdOejndX81vKUqs";
+
+const url64To8bitArr = (url64) => {
+  let padding = "=".repeat((4 - (url64.length % 4)) % 4);
+  let base64 = (url64 + padding).replace(/\-/g, "+").replace(/_/g, "/");
+
+  let rawData = buffer.atob(base64);
+  let outPutArr = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; i++) {
+    outPutArr[i] = rawData.charCodeAt(i);
+  }
+  console.log(outPutArr);
+  return outPutArr;
+};
+
+app.post("/subs", (req, res) => {
+  console.log(req.body);
+  res.end("recieved");
+});
+
+app.listen(3000, () => {
+  console.log("server is up...");
+});
